5d6410dc5b40fe70704a8fa4e7e378321d1b986d
0NGUTOR0 <ngutorugbor1@gmail.com>
2025-05-05 18:58:34 +0100

    g

--- styles.js
+++ styles.js
@@ -53,21 +53,3 @@ document.querySelectorAll('.discont').forEach(container => {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const backToTopBtn = document.getElementById("backToTop");
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-      backToTopBtn.classList.add("show");
-    } else {
-      backToTopBtn.classList.remove("show");
-    }
-  });
-
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-});
